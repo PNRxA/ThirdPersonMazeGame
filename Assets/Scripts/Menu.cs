276fd5901083e6f4ae14efe428073240101282ee
Peter Runham <pnrunham@gmail.com>
2017-08-19 10:54:06 +1000

    Added pause menu.

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -7,11 +7,15 @@ public class Menu : MonoBehaviour
     private bool splash = true;
     private bool showMainMenu = false;
     private bool showSelectCharacterMenu = false;
+    private bool showOptionsMenu = false;
+    private bool inGame = false;
+    public bool showPauseMenu = false;
     public GameObject menuCamera;
     public Texture2D splashTexture;
     private float scrH, scrW;
     public GameObject game1Scientist, game1Monster, game2Scientist, game2Monster;
     public GameObject selectCharacterPosition, mainMenuPosition;
+    public GameObject game1ScientistSpawn, game1MonsterSpawn, game2ScientistSpawn, game2MonsterSpawn;
     public float speed = 10f;
 
     void Awake()
@@ -21,15 +25,15 @@ public class Menu : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (showMainMenu)
-        {
-            float step = speed * Time.deltaTime;
-            menuCamera.transform.position = Vector3.Lerp(menuCamera.transform.position, mainMenuPosition.transform.position, step);
-        }
-        else if (showSelectCharacterMenu)
+        if (showSelectCharacterMenu)
         {
             float step = speed * Time.deltaTime;
             menuCamera.transform.position = Vector3.Lerp(menuCamera.transform.position, selectCharacterPosition.transform.position, step);
+        }
+        else
+        {
+            float step = speed * Time.deltaTime;
+            menuCamera.transform.position = Vector3.Lerp(menuCamera.transform.position, mainMenuPosition.transform.position, step);
         }
     }
 
@@ -42,7 +46,10 @@ public class Menu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame();
+        }
     }
 
     void OnGUI()
@@ -54,6 +61,10 @@ public class Menu : MonoBehaviour
         {
             Splash();
         }
+        else if (showMainMenu && showOptionsMenu)
+        {
+            OptionsMenu();
+        }
         else if (showMainMenu)
         {
             MainMenu();
@@ -61,6 +72,15 @@ public class Menu : MonoBehaviour
         else if (showSelectCharacterMenu)
         {
             SelectCharacterMenu();
+        }
+        else if (inGame && showPauseMenu && showOptionsMenu)
+        {
+            OptionsMenu();
+        }
+        else if (inGame && showPauseMenu)
+        {
+            PauseMenu();
+
         }
     }
 
@@ -87,7 +107,7 @@ public class Menu : MonoBehaviour
 
         if (GUI.Button(new Rect(scrW * 7.5f, scrH * 5.5f, scrW * 2, scrH), "Options"))
         {
-
+            showOptionsMenu = true;
         }
 
         if (GUI.Button(new Rect(scrW * 7.5f, scrH * 7, scrW * 2, scrH), "Quit"))
@@ -118,9 +138,39 @@ public class Menu : MonoBehaviour
         }
     }
 
+    void OptionsMenu()
+    {
+        if (GUI.Button(new Rect(0, 0, scrW * 16, scrH * 9), "Back"))
+        {
+            showOptionsMenu = false;
+        }
+    }
+
+    void PauseMenu()
+    {
+        GUI.Box(new Rect(scrW * 7, scrH * 2, scrW * 3, scrH), "Game Paused");
+
+        if (GUI.Button(new Rect(scrW * 7.5f, scrH * 4, scrW * 2, scrH), "Resume"))
+        {
+            PauseGame();
+        }
+
+        if (GUI.Button(new Rect(scrW * 7.5f, scrH * 5.5f, scrW * 2, scrH), "Options"))
+        {
+            showOptionsMenu = true;
+        }
+
+        if (GUI.Button(new Rect(scrW * 7.5f, scrH * 7, scrW * 2, scrH), "Back to main menu"))
+        {
+            PauseGame();
+            ToMenu();
+        }
+    }
+
     void StartGame(bool asScientist)
     {
         showMainMenu = false;
+        inGame = true;
         menuCamera.SetActive(false);
         if (asScientist)
         {
@@ -132,5 +182,38 @@ public class Menu : MonoBehaviour
             game2Scientist.SetActive(true);
             game2Monster.SetActive(true);
         }
+    }
+
+    void PauseGame()
+    {
+        showPauseMenu = !showPauseMenu;
+
+        if (showPauseMenu)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+
+    void ToMenu()
+    {
+        showMainMenu = true;
+        showSelectCharacterMenu = false;
+        showOptionsMenu = false;
+        inGame = false;
+        showPauseMenu = false;
+
+        menuCamera.SetActive(true);
+        game1Scientist.transform.position = game1ScientistSpawn.transform.position;
+        game1Scientist.SetActive(false);
+        game1Monster.transform.position = game1MonsterSpawn.transform.position;
+        game1Monster.SetActive(false);
+        game2Scientist.transform.position = game2ScientistSpawn.transform.position;
+        game2Scientist.SetActive(false);
+        game2Monster.transform.position = game2MonsterSpawn.transform.position;
+        game2Monster.SetActive(false);
     }
 }
